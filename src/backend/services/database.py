c5b01e8cd556e5a292dba04103cc7e3855c00407
lnosiqueira <lnosiqueira@gmail.com>
2026-03-26 16:04:52 -0300

    fix: corrige path do banco no render

--- src/backend/services/database.py
+++ src/backend/services/database.py
@@ -1,6 +1,7 @@
-import sqlite3
+import os
 
-DB_PATH = "database/totemflex.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "../../../database/totemflex.db")
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
