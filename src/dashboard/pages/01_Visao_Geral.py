cd474c3ca61684ea43b912d40072b685d0409ba5
lnosiqueira <lnosiqueira@gmail.com>
2026-04-10 14:56:53 -0300

    gemini 1.5

--- src/dashboard/pages/01_Visao_Geral.py
+++ src/dashboard/pages/01_Visao_Geral.py
@@ -9,7 +9,7 @@ import os
 # =========================
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 def gerar_insight_ia(df):
     try:
